6930599a340aa7c9e2d12e8b3323cfdc9d559f00
Gennady Ershov <ershov2007@gmail.com>
2024-06-10 22:38:16 +0800

    fixed typing

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,17 @@
-def filter_by_state(transactions: list, state: str = "EXECUTED") -> list:
+from typing import Optional
+
+
+def filter_by_state(operations: list[dict], state: Optional[str] = "EXECUTED") -> list[dict]:
     """Фильтрует транзакции по состоянию"""
-    return [elem for elem in transactions if elem["state"] == state]
+    return [elem for elem in operations if elem["state"] == state]
 
 
-def filter_by_date(transactions: list, ascending: bool = False) -> list:
+def filter_by_date(operations: list[dict], ascending: bool = False) -> list[dict]:
     """Фильтрует транзакции по дате"""
-    return sorted(transactions, key=lambda date: date.get("date"), reverse=ascending)
+    return sorted(operations, key=lambda date: date.get("date"), reverse=ascending)
 
 
-transactions = [
+operations = [
     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
@@ -16,5 +19,5 @@ transactions = [
 ]
 
 if __name__ == "__main__":
-    print(filter_by_state(transactions, state="CANCELED"))
-    print(filter_by_date(transactions))
+    print(filter_by_state(actions, state="CANCELED"))
+    print(filter_by_date(actions))
