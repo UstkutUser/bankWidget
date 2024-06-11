c15517dec0db11de8583004512f293ef2f6b1cbc
Gennady Ershov <ershov2007@gmail.com>
2024-06-12 01:34:58 +0800

    fixed processing.py

--- src/processing.py
+++ src/processing.py
@@ -10,7 +10,3 @@ def filter_by_date(operations: list[dict], descending: bool = True) -> list[dict
     """Фильтрует транзакции по дате"""
     return sorted(operations, key=lambda date: date.get("date"), reverse=descending)
 
-
-if __name__ == "__main__":
-    print(filter_by_state(operations, state="CANCELED"))
-    print(filter_by_date(operations))
