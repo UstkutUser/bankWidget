9bf189442137879bb859a1d49f9cf0c354a975c5
Gennady Ershov <ershov2007@gmail.com>
2024-06-16 19:44:38 +0800

    homework_11_1: made filter_by_currency

---
+++ src/generators.py
@@ -0,0 +1,13 @@
+from src.transactions import transactions
+def filter_by_currency(transactions: list, currency: str) -> iter:
+    """Возвращает итератор, который выдает по очереди операции в указанной валюте"""
+    return filter(lambda x: x["operationAmount"]["currency"]["name"] == currency, transactions)
+
+
+usd_transactions = filter_by_currency(transactions, "USD")
+
+try:
+    for _ in range(len(transactions)):
+        print(next(usd_transactions)["id"])
+except StopIteration:
+    pass
