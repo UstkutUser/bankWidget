3a11a3e094e11a93fd105b7ef72f5426e7b80e27
Gennady Ershov <ershov2007@gmail.com>
2024-06-16 23:03:00 +0800

    made transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -1,12 +1,19 @@
-from typing import Iterator, Generator, Dict
-
-from src.data import transactions
+from typing import Generator, Dict
 
 
-def filter_by_currency(transactions: list[Dict], currency: str = "USD") -> Generator[Dict[str, object], None, None]:
+def filter_by_currency(
+    transactions: list[Dict], currency: str = "USD"
+) -> Generator[Dict[str, object], None, None]:
     """Возвращает итератор, который выдает по очереди операции в указанной валюте"""
     for transaction in transactions:
         if transaction["operationAmount"]["currency"]["code"] == currency:
             yield transaction
 
+
+def transaction_descriptions(
+    transactions: list[Dict],
+) -> Generator[Dict[str, object], None, None]:
+    """Возвращает описание каждой операции по очереди"""
+    for transaction in transactions:
+        yield transaction["description"]
 
