f5888736daf86f2f4199b204001fee9e6d234fce
Gennady Ershov <ershov2007@gmail.com>
2024-06-20 22:34:44 +0800

    done tests, README.md

--- src/decorators.py
+++ src/decorators.py
@@ -1,6 +1,13 @@
-def log(filename=""):
-    def wrapper(function):
-        def inner(*args, **kwargs):
+from functools import wraps
+from typing import Any, Callable
+
+
+def log(filename="") -> Callable:
+    """Логирует в файл или консоль вызов функций-обработчиков и результаты их работы"""
+
+    def wrapper(function: Callable) -> Callable:
+        @wraps(function)
+        def inner(*args: Any, **kwargs: Any) -> Any:
             try:
                 message_ok = f"{function.__name__} ok"
                 result = function(*args, **kwargs)
@@ -11,18 +18,24 @@ def log(filename=""):
                     print(f"{message_ok}")
                 return result
             except Exception as e:
-                message_err = f"{function.__name__} error: {e}. Inputs: {args}, {kwargs}"
+                message_err = (
+                    f"{function.__name__} error: {e}. Inputs: {args}, {kwargs}"
+                )
                 if filename:
                     with open(filename, "w") as file:
                         file.write(f"{message_err}\n")
                 else:
                     print(f"{message_err}")
+
         return inner
+
     return wrapper
 
 
-@log(filename="log.txt")
-def func(a, b):
-    return a / b
+@log(filename="mylog.txt")
+def my_function(x, y):
+    return x / y
 
-func(2,0)
+
+if __name__ == "__main__":
+    my_function(10, 2)
