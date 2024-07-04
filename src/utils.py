bbc1b707345b3167692b40a9de8d8f69b3cc0971
Gennady Ershov <ershov2007@gmail.com>
2024-07-05 00:15:35 +0700

    started get_transaction_amount

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,6 @@
 import json
 from typing import Dict, Any
 
-
-
 """
 Реализуйте функцию, которая принимает на вход путь до JSON-файла и 
 возвращает список словарей с данными о финансовых транзакциях. Если файл 
@@ -10,9 +8,11 @@ from typing import Dict, Any
 Функцию поместите в модуль utils. 
 Файл с данными о финансовых транзациях operations.json поместите в 
 директорию data/ в корне проекта."""
+
+
 def get_transactions_from_file(path: str) -> list[Dict] | Any:
     """Принимает на вход путь до JSON-файла и
-возвращает список словарей с данными о финансовых транзакциях."""
+    возвращает список словарей с данными о финансовых транзакциях."""
     try:
         with open(path, "r", encoding="utf-8") as file:
             try:
@@ -26,6 +26,39 @@ def get_transactions_from_file(path: str) -> list[Dict] | Any:
     return transactions_data
 
 
-if __name__ == '__main__':
-    transactions = get_transactions_from_file("c:\\python\\bankWidget\\data\\operations.json")
+if __name__ == "__main__":
+    transactions = get_transactions_from_file(
+        "c:\\python\\bankWidget\\data\\operations.json"
+    )
     print(transactions)
+
+"""Реализуйте функцию, которая принимает на вход транзакцию 
+и возвращает сумму транзакции (amount) в рублях, тип данных — 
+float. Если транзакция была в USD или EUR, происходит обращение 
+к внешнему API для получения текущего курса валют и конвертации 
+суммы операции в рубли. Для конвертации валюты воспользуйтесь 
+Exchange Rates Data API: https://apilayer.com/exchangerates_data-api. 
+Функцию конвертации поместите в модуль external_api."""
+
+
+def get_transaction_amount(transaction: Dict) -> float:
+    """Функция возвращает сумму транзакции в рублях, при необходимости конвертирует в рубли"""
+    if not transaction:
+        return 0.0
+    elif "operationAmount" in transaction:
+
+        return transaction["operationAmount"].get("amount")
+
+
+if __name__ == "__main__":
+    get_transaction_amount(
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {"name": "руб.", "code": "RUB"},
+            },
+        }
+    )
