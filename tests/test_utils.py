372b4a3046993dcc36c07eaaa8f191ba67c38013
Gennady Ershov <ershov2007@gmail.com>
2024-07-05 10:24:59 +0700

    fixed test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -21,7 +21,7 @@ def test_get_transaction_amount():
 
 
 @patch("src.utils.currency_conversion")
-def test_currency_conversion_usd(mocked_conversion):
+def test_currency_conversion(mocked_conversion):
     mocked_conversion.return_value = 10
     result = get_transaction_amount(
             {
@@ -38,3 +38,38 @@ def test_currency_conversion_usd(mocked_conversion):
         }
     )
     assert result == "10"
+
+
+def test_currency_conversion_called_once():
+    """Проверяет, была ли функция вызвана с определенными аргументами"""
+    with patch("src.utils.currency_conversion") as curr_conv:
+        curr_conv.return_value = 100
+        transaction = {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {"amount": "10", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        }
+        assert get_transaction_amount(transaction) == 100
+        curr_conv.assert_called_once_with("10", "USD")
+
+
+def test_get_transaction_amount_without_amount():
+    """Проверяет, если в транзакции нет суммы"""
+    transaction = {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702"
+    }
+    assert get_transaction_amount(transaction) == 0.0
+
+
+def test_get_transaction_amount_empty_transaction():
+    """Проверяет, если транзакции нет"""
+    assert get_transaction_amount("") == 0.0
